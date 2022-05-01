ad1d8f1f5c047091928c87c356e7487ea26d2c7c
Matěj Konopík <xkonop03@vutbr.cz>
2022-05-01 14:29:36 +0200

    Added user ride facade

---
+++ carpool/carpool.BL/Facades/UserRideFacade.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Carpool.BL.Models;
+using Carpool.DAL.Entities;
+using Carpool.DAL.UnitOfWork;
+
+namespace Carpool.BL.Facades;
+
+public class UserRideFacade : CRUDFacade<UserRideEntity, UserRideDetailModel, UserRideDetailModel>
+{
+    public UserRideFacade(IUnitOfWorkFactory unitOfWorkFactory, IMapper mapper) : base(unitOfWorkFactory, mapper)
+    {
+    }
+    public override Task<UserRideDetailModel> SaveAsync(UserRideDetailModel model)
+        {
+            //TEST
+            return base.SaveAsync(model);
+        }
+}
