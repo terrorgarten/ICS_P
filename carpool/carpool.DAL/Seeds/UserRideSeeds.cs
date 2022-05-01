8d0ee7d39408c2901c52b9db5559ca108f4d21f6
Matěj Konopík <xkonop03@vutbr.cz>
2022-05-01 21:10:39 +0200

    Fixed User ride seeds

--- carpool/carpool.DAL/Seeds/UserRideSeeds.cs
+++ carpool/carpool.DAL/Seeds/UserRideSeeds.cs
@@ -1,5 +1,4 @@
-﻿using Carpool.Common.Enums;
-using Carpool.DAL.Entities;
+﻿using Carpool.DAL.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace Carpool.DAL.Seeds
@@ -7,29 +6,29 @@ namespace Carpool.DAL.Seeds
     public static class UserRideSeeds
     {
         public static readonly UserRideEntity UserRide1 = new(
-            Id: Guid.Parse(input: "DC935A57-A6B1-4806-B3D0-14FD166CA7AA"),
-            PassengerId: UserSeeds.SecondUser.Id,
+            Id: Guid.Parse(input: "3C5391D1-624E-4CEB-A93C-1A6F4980CEF7"),
+            PassengerId: UserSeeds.FirstUser.Id,
             RideId: RideSeeds.Ride1.Id)
         {
-            Passenger = UserSeeds.SecondUser,
-            Ride = RideSeeds.Ride1,
+            Passenger = UserSeeds.FirstUser,
+            Ride = RideSeeds.Ride1
         };
 
         public static readonly UserRideEntity UserRide2 = new(
-            Id: Guid.Parse(input: "F87FF297-C678-4AEA-B5DB-12AD3FDC6E87"),
-            PassengerId: UserSeeds.FirstUser.Id,
+            Id: Guid.Parse(input: "E29C9F5C-F879-4BD9-9B3D-0AA5327E6730"),
+            PassengerId: UserSeeds.SecondUser.Id,
             RideId: RideSeeds.Ride2.Id)
         {
-            Passenger = UserSeeds.FirstUser,
-            Ride = RideSeeds.Ride2,
+            Passenger = UserSeeds.SecondUser,
+            Ride = RideSeeds.Ride2
         };
 
-        public static void Seed(ModelBuilder modelBuilder)
+        public static void Seed(this ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<RideEntity>().HasData(
-                UserRide1 with { Passenger = null, Ride = null },
-                UserRide2 with { Passenger = null, Ride = null });
-
+            modelBuilder.Entity<UserRideEntity>().HasData(
+            UserRide1 with { Passenger = null, Ride = null },
+            UserRide2 with { Passenger = null, Ride = null }
+            );
         }
     }
 }
